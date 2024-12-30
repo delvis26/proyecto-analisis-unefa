e30b5b0434b348f692e01f2587759cbf5d474511
Delvis <delvisalexander26@gmail.com>
2024-12-30 01:33:24 +0100

    Update #2 30/12/2024

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -1,11 +1,19 @@
 import FormLogin from "@/components/form-login";
 import type { Metadata } from "next";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Colegio Barquisimeto",
 };
 
-export default function Login() {
+export default async function Login() {
+  const cookieStore = await cookies()
+
+  const token = cookieStore.get("access_token")
+
+  if(token) redirect('/')
+
   return (
     <div className="w-full min-h-screen bg-gray-200 flex flex-col items-center justify-center px-5">
       <FormLogin />
