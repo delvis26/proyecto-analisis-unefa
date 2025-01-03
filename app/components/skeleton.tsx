c97d9d881ae387c2d78276d5f34dea238f6819a1
Delvis <delvisalexander26@gmail.com>
2025-01-03 20:29:22 +0100

    Fix 4

--- app/components/skeleton.tsx
+++ app/components/skeleton.tsx
@@ -1,6 +1,6 @@
 export default function TextSkeleton({h}:{h:string}) {
     return (
-        <span style={{height: h}} className={`w-full bg-gray-200 animate-pulse p-[1px] rounded-lg inline-flex`}>
+        <span style={{height: h}} className="w-full bg-gray-200 animate-pulse p-[1px] rounded-lg inline-flex">
         </span>
     )
 }
