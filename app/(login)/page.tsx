ca705e7694d28edb183c7b6c783c13db132fc1e9
Delvis <delvisalexander26@gmail.com>
2025-01-03 04:07:32 +0100

    fix login

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -12,7 +12,7 @@ export default async function Login() {
 
   const token = cookieStore.get("access_token")
 
-  if(token) redirect('/')
+  if(token) return redirect('/home')
 
   return (
     <div className="w-full min-h-screen bg-gray-200 flex flex-col items-center justify-center px-5">
