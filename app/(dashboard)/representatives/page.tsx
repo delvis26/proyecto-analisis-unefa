cf01eaf1f20c2e944a103d13061dc554b070650a
Delvis <delvisalexander26@gmail.com>
2025-01-25 06:31:03 +0100

    Update final final

--- app/(dashboard)/representatives/page.tsx
+++ app/(dashboard)/representatives/page.tsx
@@ -115,7 +115,7 @@ export default function Representatives() {
         <div className="flex flex-row *:truncate *:flex-1 px-4 py-2 gap-2 bg-black/5 font-semibold rounded-lg">
           <div>Nombre completo</div>
           <div>Cedula</div>
-          <div className="flex justify-center">Status</div>
+          <div className="flex justify-center">Estado</div>
           <div className="flex justify-end">Acciones</div>
         </div>
 
