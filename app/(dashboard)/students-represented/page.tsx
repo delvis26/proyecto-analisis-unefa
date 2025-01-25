d77fb892aa8785493bafae9f7e28bf5353ea9760
Delvis <delvisalexander26@gmail.com>
2025-01-25 06:47:24 +0100

    Final final final

--- app/(dashboard)/students-represented/page.tsx
+++ app/(dashboard)/students-represented/page.tsx
@@ -82,7 +82,7 @@ export default function StudensRepresented() {
           <div className="flex flex-row *:truncate *:flex-1 px-4 py-2 gap-2 bg-black/5 font-semibold rounded-lg">
             <div>Nombre completo</div>
             <div>Genero</div>
-            <div className="flex justify-center">Status</div>
+            <div className="flex justify-center">Estado</div>
             <div className="flex justify-end">Acciones</div>
           </div>
         </div>
